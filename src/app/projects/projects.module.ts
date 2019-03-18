1978b62f38ae45807208108e72d075ddea23531a
el-davo <david.ahern@emc.com>
2019-03-18 18:19:20 +0000

    Fixing e2e

--- src/app/projects/projects.module.ts
+++ src/app/projects/projects.module.ts
@@ -7,11 +7,20 @@ import { StoreModule } from '@ngrx/store';
 import { reducer } from './projects.reducer';
 import { EffectsModule } from '@ngrx/effects';
 import { ProjectsEffects } from './projects.effects';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ProjectsComponent
+  },
+];
 
 @NgModule({
   declarations: [ProjectsComponent, ProjectsListComponent],
   imports: [
     CommonModule,
+    RouterModule.forChild(routes),
     StoreModule.forFeature('projects', reducer),
     EffectsModule.forFeature([ProjectsEffects]),
 
