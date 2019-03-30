e15ad74424decd36e7e1d3d8725fd96decc162fc
el-davo <david.ahrn26@gmail.com>
2019-03-30 13:27:15 +0000

    Making things a bit smoother

--- src/app/tasks/create-task-modal/create-task-modal.component.ts
+++ src/app/tasks/create-task-modal/create-task-modal.component.ts
@@ -1,10 +1,10 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Store } from '@ngrx/store';
+import {Component, Inject, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Store} from '@ngrx/store';
 import * as appActions from '../../app.actions';
 import * as actions from '../tasks.actions';
-import { ModuleState } from '../module.state';
-import { MAT_DIALOG_DATA } from '@angular/material';
+import {ModuleState} from '../module.state';
+import {MAT_DIALOG_DATA} from '@angular/material';
 
 @Component({
   selector: 'app-create-task-modal',
@@ -24,7 +24,7 @@ export class CreateTaskModalComponent implements OnInit {
       uri: [null, [Validators.required]],
       totalSimulatedUsers: [1000, [Validators.required]],
       runTime: [30, [Validators.required]],
-      cfApi: [null, [Validators.required]],
+      cfApi: ['https://api.run.pivotal.io', [Validators.required]],
       cfUser: [null, [Validators.required]],
       cfPass: [null, [Validators.required]],
       cfOrg: [null, [Validators.required]],
